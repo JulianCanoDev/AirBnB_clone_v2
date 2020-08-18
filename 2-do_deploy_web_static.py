9af92f83aea278cde767f18db807a0f146ef9fd8
julian <juliancano.dev@gmail.com>
2020-08-18 07:39:16 -0500

    Task 0

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -18,20 +18,20 @@ def do_deploy(archive_path):
         return False
     with api.cd('/tmp'):
         base = os.path.basename(archive_path)
-        root, ext = os.path.splitext(base)
+        root = os.path.splitext(base)
         outpath = '/data/web_static/releases/{}'.format(root)
         try:
             putpath = api.put(archive_path)
             if files.exists(outpath):
-                api.run('rm -rdf {}'.format(outpath))
-            api.run('mkdir -p {}'.format(outpath))
-            api.run('tar -xzf {} -C {}'.format(putpath[0], outpath))
-            api.run('rm -f {}'.format(putpath[0]))
-            api.run('mv -u {}/web_static/* {}'.format(outpath, outpath))
-            api.run('rm -rf {}/web_static'.format(outpath))
-            api.run('rm -rf /data/web_static/current')
-            api.run('ln -sf {} /data/web_static/current'.format(outpath))
-            print('New version deployed!')
+                api.run('sudo rm -rdf {}'.format(outpath))
+                api.run('sudo mkdir -p {}'.format(outpath))
+                api.run('sudo tar -xzf {} -C {}'.format(putpath[0], outpath))
+                api.run('sudo rm -f {}'.format(putpath[0]))
+                api.run('sudo mv -u {}/web_static/* {}'.format(outpath, outpath))
+                api.run('sudo rm -rf {}/web_static'.format(outpath))
+                api.run('sudo rm -rf /data/web_static/current')
+                api.run('sudo ln -sf {} /data/web_static/current'.format(outpath))
+                print('New version deployed!')
         except:
             return False
         else:
