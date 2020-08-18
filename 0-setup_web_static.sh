865e7602979158855e898f7e8804e5f7a2077ceb
julian <juliancano.dev@gmail.com>
2020-08-17 19:15:39 -0500

    Mangi

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 # Bash script that sets up your web servers for the deployment of web_static
-sudo apt-get -y update
-sudo apt-get -y install nginx
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-echo "simple content, to test your Nginx configuration" | sudo tee /data/web_static/releases/test/index.html
-ln -sf  /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-content="location /hbnb_static/ {\nalias /data/web_static/current;\n}"
-echo "$content" | sudo tee -a /etc/nginx/sites-enabled/default
-sudo service nginx restart
+apt-get update && \
+apt-get install -y nginx && \
+mkdir -p -m=755 /data/web_static/{releases/test,shared} || exit 0
+echo 'Holberton School for win!' > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -hR ubuntu:ubuntu /data/
+insert='\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;}'
--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,23 +3,25 @@
      from the contents of the web_static folder of
      your AirBnB Clone repo, using the function do_pack.
 """
-from fabric.api import local
+from fabric import api
 import os.path
 from datetime import datetime
 
 
 def do_pack():
-    """ script that generates a .tgz archive
-    from the contents of the web_static"""
-    date_now = datetime.utcnow()
-    file_date_format = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-                                                        date_now.year,
-                                                        date_now.month,
-                                                        date_now.day,
-                                                        date_now.hour,
-                                                        date_now.minute,
-                                                        date_now.second)
-    local("sudo mkdir -p versions")
-    local("sudo tar -cvzf {} web_static".format(file_date_format))
-
-    return file_date_format
+     """ script that generates a .tgz archive
+     from the contents of the web_static"""
+     with api.settings(warn_only=True):
+          isdir = os.path.isdir('versions')
+          if not isdir:
+               mkdir = api.local('mkdir versions')
+               if mkdir.failed:
+                    return None
+          suffix = datetime.now().strftime('%Y%m%d%M%S')
+          path = 'versions/web_static_{}.tgz'.format(suffix)
+          tar = api.local('tar -cvzf {} web_static'.format(path))
+          if tar.failed:
+               return None
+          size = os.stat(path).st_size
+          print('web_static packed: {} -> {}Bytes'.format(path, size))
+          return path
