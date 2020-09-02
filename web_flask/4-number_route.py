ee86b5da1c612f6e50ed7cd110e58f08a364e0c1
julian <juliancano.dev@gmail.com>
2020-09-02 10:39:58 -0500

    task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Flash app"""
+from flask import Flask
+app = Flask('web_flask')
+
+
+@app.route('/', strict_slashes=False)
+def hello_route_1():
+    """Return string 1"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_route_2():
+    """Return string 2"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hello_route_3(text):
+    """Return string 3"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/<text>')
+@app.route('/python/', defaults={'text': 'is cool'})
+def hello_route_4(text):
+    """Return string 4"""
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>')
+def hello_route_5(n):
+    """Return number 5"""
+    return '{:d} is a number'.format(n)
+
+
+if __name__ == "__main__":
+    """Running point"""
+    app.run(host='0.0.0.0', port=5000)
