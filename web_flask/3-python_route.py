d742baf46de124095fa2f124fb7e8bcc15e392a5
julian <juliancano.dev@gmail.com>
2020-09-02 10:24:23 -0500

    task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -16,10 +16,16 @@ def hello_route_2():
     return 'HBNB'
 
 
+@app.route('/c/<text>', strict_slashes=False)
+def hello_route_3(text):
+    """Return string 3"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
 @app.route('/python/(<text>)', strict_slashes=False)
 def hello_route_4(text="is cool"):
-    """Return string 2"""
-    return 'C {}'.format(text.replace('_', ' '))
+    """Return string 4"""
+    return 'Python {}'.format(text.replace('_', ' '))
 
 
 if __name__ == "__main__":
